1210e6b8c32492bc11f0cea24ad7f6e1eec9eeb1
Chinwike <maduabuchichizzy@gmail.com>
2024-03-19 00:40:16 +0100

    first commit

---
+++ components/firebase/firebase.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { /* connectFirestoreEmulator, */ getFirestore } from 'firebase/firestore';
+import /* connectStorageEmulator, getStorage */ 'firebase/storage';
+// import { isDev } from '../isDev';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FB_APIKEY,
+    authDomain: process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FB_PROJECTID,
+    storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FB_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FB_APPID,
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
+
+export const firestore = getFirestore(firebaseApp);
+export const baseBucketName = 'mini-extensions';
+
+/* if (isDev) {
+    connectFirestoreEmulator(firestore, '127.0.0.1', 8081);
+} */
